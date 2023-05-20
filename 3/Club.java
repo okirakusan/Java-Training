52ad5c138e7edfe90f2edaa92564ff13d029ebeb
okirakusan <oimbt0049@gmail.com>
2023-05-20 21:18:03 +0900

    ✨ 抽象クラスの継承とポリモーフィズム（多態化）

---
+++ 3/Club.java
@@ -0,0 +1,10 @@
+public abstract class Club {
+    private String name;  //部活動名
+    public Club(String name){
+        this.name = name;
+    }
+    public void display(){
+        System.out.println("部活動：" + name);
+    }
+    public abstract void practice();
+}
---
+++ 3/StuSample6.java
@@ -0,0 +1,9 @@
+public class StuSample6 {
+    public static void main (String[] args){
+        Football fb = new Football("サッカー部");
+
+        Student6 stu1 = new Student6("桜井",fb);
+        stu1.display();
+        stu1.practice();
+    }
+}
