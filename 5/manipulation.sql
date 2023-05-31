2fa8dbfd56c365cc09f0aa9dca11a1c881b8c63f
okirakusan <oimbt0049@gmail.com>
2023-05-31 22:26:37 +0900

    ✨ MySQL:DB定義、データ操作

---
+++ 5/manipulation.sql
@@ -0,0 +1,13 @@
+SELECT * FROM student;
+
+INSERT INTO student VALUES(1,'菅原',80);
+
+SELECT * FROM student;
+
+SELECT no,score * 0.5 FROM student;
+
+UPDATE student SET score = score + 5;
+
+SELECT * FROM student;
+
+DELETE FROM student;
